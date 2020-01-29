87c22fc5c634366076b07c2159235860813b26d9
Alex <Alex@DESKTOP-ENSNID7>
2020-01-29 09:35:32 +0300

    123

---
+++ ModalWindowContainerComponent/Texts.cs
@@ -0,0 +1,12 @@
+﻿namespace ModalWindowContainerComponent
+{
+    public struct Texts
+    {
+        public const string OverlayClass = "bm-overlay";
+        public const string ModalClass = "blazor-modal";
+        public const string HeaderClass = "bm-header";
+        public const string TitleClass = "bm-title";
+        public const string ButtonCloseClass = "bm-close";
+        public const string ContentClass = "bm-content";
+    }
+}
