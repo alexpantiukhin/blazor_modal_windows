f58a8a1bb694a71924b65dff79c5819ff3676439
Alex <Alex@DESKTOP-ENSNID7>
2020-01-29 08:53:59 +0300

    123

--- ModalWindowComponent/obj/Debug/netstandard2.0/ModalWindowComponent.AssemblyInfo.cs
+++ ModalWindowComponent/obj/Debug/netstandard2.0/ModalWindowComponent.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("ModalWindowComponent")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Pantiukhin Aleksandr, Chris Sainty")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("ModalWindowComponent")]
-[assembly: System.Reflection.AssemblyTitleAttribute("ModalWindowComponent")]
+[assembly: System.Reflection.AssemblyProductAttribute("BlazorModalWindowComponent")]
+[assembly: System.Reflection.AssemblyTitleAttribute("BlazorModalWindowComponent")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
