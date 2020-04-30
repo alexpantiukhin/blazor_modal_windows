33183bfb9b4de58a8ef17402670efa787841ed91
Александр Пантюхин <alexander.pantuhin@libroplanta.com>
2020-04-30 19:21:32 +0300

    1.0.5

---
+++ BlazorModalWindowComponent/TagHelper.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BlazorModalWindowComponent
+{
+    public static class TagHelper
+    {
+        public static HtmlString AddModalWindowStyle(this IHtmlHelper html, string[] items)
+        {
+            string result = "<link href='_content/BlazorModalWindowComponent/index.css' rel='stylesheet' />";
+            return new HtmlString(result);
+        }
+    }
+}
