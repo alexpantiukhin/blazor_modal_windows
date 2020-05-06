aedc8f82f109ae7ea89d6a186308d7412d4c8f19
Александр Пантюхин <alexander.pantuhin@libroplanta.com>
2020-05-06 14:32:33 +0300

    1.0.8

--- BlazorModalWindowComponent/TagHelper.cs
+++ BlazorModalWindowComponent/TagHelper.cs
@@ -5,9 +5,10 @@ namespace BlazorModalWindowComponent
 {
     public static class TagHelper
     {
-        public static HtmlString AddModalWindowStyle(this IHtmlHelper html, string[] items)
+        public static HtmlString AddModalWindowStyle(this IHtmlHelper html, params string[] items)
         {
             string result = "<link href='_content/BlazorModalWindowComponent/index.css' rel='stylesheet' />";
+            result += items == null ? null : string.Join("", items);
             return new HtmlString(result);
         }
     }
